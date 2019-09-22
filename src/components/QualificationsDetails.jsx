98e20be3fee0ff02392bfe557530ee44832065ce
Jeremy Chui <jeremy@jchui.me>
2019-09-22 02:06:44 +0100

    Qualifications defaultValue fix

--- src/components/QualificationsDetails.jsx
+++ src/components/QualificationsDetails.jsx
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import { Form, Button, Progress, Icon } from 'semantic-ui-react';
 
+function handleDefaultChecked(qualifications, current) {
+  if (qualifications === current) {
+    return true;
+  }
+}
+
 class QualificationsDetails extends Component{
 
     saveAndContinue = (e) => {
@@ -14,6 +20,7 @@ class QualificationsDetails extends Component{
     }
 
     render(){
+
         return(
             <Form className="calculatorForm">
                 <Progress percent={33} active />
@@ -22,25 +29,27 @@ class QualificationsDetails extends Component{
                     <img src={process.env.PUBLIC_URL + "/img/graduation.png"} height="200px" alt="degree"/>
 
                     <div className="radios">
-                        <input 
-                            type="radio" 
-                            name="rGroup" 
+                        <input
+                            type="radio"
+                            name="rGroup"
                             value={0}
-                            id="r1" 
+                            id="r1"
                             onChange={this.props.handleChange('qualifications')}
-                            defaultChecked /><label className="radio" htmlFor="r1">
+                            defaultChecked={handleDefaultChecked(this.props.values.qualifications, 0)} />
+                        <label className="radio" htmlFor="r1">
                             <p>Primary Medical Qualification only</p>
-                            <p>3rd class BMedSci awarded at University of Nottingham 
+                            <p>3rd class BMedSci awarded at University of Nottingham
                             <span className="points">+ 0 points</span>
                             </p>
                         </label>
-                        
-                        <input 
-                            type="radio" 
-                            name="rGroup" 
-                            value={1} 
-                            id="r2" 
-                            onChange={this.props.handleChange('qualifications')} />
+
+                        <input
+                            type="radio"
+                            name="rGroup"
+                            value={1}
+                            id="r2"
+                            onChange={this.props.handleChange('qualifications')}
+                            defaultChecked={handleDefaultChecked(this.props.values.qualifications, 1)} />
                         <label className="radio" htmlFor="r2">
                             <p>3rd class honours degree</p>
                             <p>Unclassified honours degree</p>
@@ -49,12 +58,13 @@ class QualificationsDetails extends Component{
                             </p>
                         </label>
 
-                        <input 
-                            type="radio" 
-                            name="rGroup" 
-                            value={2} 
-                            id="r3" 
-                            onChange={this.props.handleChange('qualifications')} />
+                        <input
+                            type="radio"
+                            name="rGroup"
+                            value={2}
+                            id="r3"
+                            onChange={this.props.handleChange('qualifications')}
+                            defaultChecked={handleDefaultChecked(this.props.values.qualifications, 2)} />
                         <label className="radio" htmlFor="r3">
                             <p>2.2 class honours degree</p>
                             <p>2.1 class BMedSci awarded at University of Nottingham
@@ -62,12 +72,13 @@ class QualificationsDetails extends Component{
                             </p>
                         </label>
 
-                        <input 
-                            type="radio" 
-                            name="rGroup" 
-                            value={3} 
-                            id="r4" 
-                            onChange={this.props.handleChange('qualifications')} />
+                        <input
+                            type="radio"
+                            name="rGroup"
+                            value={3}
+                            id="r4"
+                            onChange={this.props.handleChange('qualifications')}
+                            defaultChecked={handleDefaultChecked(this.props.values.qualifications, 3)} />
                         <label className="radio" htmlFor="r4">
                             <p>2.1 class honours degree</p>
                             <p>1st class BMedSci awarded at University of Nottingham
@@ -75,12 +86,13 @@ class QualificationsDetails extends Component{
                             </p>
                         </label>
 
-                        <input 
-                            type="radio" 
-                            name="rGroup" 
-                            value={4} 
-                            id="r5" 
-                            onChange={this.props.handleChange('qualifications')} />
+                        <input
+                            type="radio"
+                            name="rGroup"
+                            value={4}
+                            id="r5"
+                            onChange={this.props.handleChange('qualifications')}
+                            defaultChecked={handleDefaultChecked(this.props.values.qualifications, 4)} />
                         <label className="radio" htmlFor="r5">
                             <p>1st class honours degree</p>
                             <p>Postgraduate Master's degree (level 7 only), e.g. MPhil, MSc, MPharm</p>
@@ -90,12 +102,13 @@ class QualificationsDetails extends Component{
                             </p>
                         </label>
 
-                        <input 
-                            type="radio" 
-                            name="rGroup" 
-                            value={5} 
-                            id="r6" 
-                            onChange={this.props.handleChange('qualifications')} />
+                        <input
+                            type="radio"
+                            name="rGroup"
+                            value={5}
+                            id="r6"
+                            onChange={this.props.handleChange('qualifications')}
+                            defaultChecked={handleDefaultChecked(this.props.values.qualifications, 5)} />
                         <label className="radio" htmlFor="r6">
                             <p>Doctoral degree (PhD, DPhil, etc)
                             <span className="points">+ 5 points</span>
