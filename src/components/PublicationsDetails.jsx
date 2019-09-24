4f1385c8c29232646a96ef7bb8f8f25bce75e469
Jeremy <jeremy@jchui.me>
2019-09-24 15:03:26 +0100

    Fix Publications Scoring

--- src/components/PublicationsDetails.jsx
+++ src/components/PublicationsDetails.jsx
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import { Form, Button, Progress, Icon } from 'semantic-ui-react';
 
+function handleDefaultChecked(qualifications, current) {
+    if (qualifications === current) {
+      return true;
+    }
+  }
+
 class PublicationsDetails extends Component{
 
     saveAndContinue = (e) => {
@@ -22,19 +28,46 @@ class PublicationsDetails extends Component{
                     <p>Do you have any publications? (2 points)</p>
                     <img src={process.env.PUBLIC_URL + "/img/take_a_note_2.png"} height="200px" alt="report"/>
                     <br/>
-                    <Button.Group size='large'>
-                        <Button
-                            value={2}
-                            onClick={this.props.handleChange('publications')}
-                            active={!values.publications ? false : true}
-                        >Yes</Button>
-                        <Button.Or />
-                        <Button
+                    <div className="radios">
+                        <input
+                            type="radio"
+                            name="rGroup"
                             value={0}
-                            onClick={this.props.handleChange('publications')}
-                            active={!values.publications ? true : false}
-                        >No</Button>
-                    </Button.Group>
+                            id="r1"
+                            onChange={this.props.handleChange('publications')}
+                            defaultChecked={handleDefaultChecked(this.props.values.publications, 0)} />
+                        <label className="radio" htmlFor="r1">
+                            <p>No publications
+                            <span className="points">+ 0 points</span>
+                            </p>
+                        </label>
+
+                        <input
+                            type="radio"
+                            name="rGroup"
+                            value={1}
+                            id="r2"
+                            onChange={this.props.handleChange('publications')}
+                            defaultChecked={handleDefaultChecked(this.props.values.publications, 1)} />
+                        <label className="radio" htmlFor="r2">
+                            <p><strong>x1</strong> Educational research paper published in a peer-reviewed journal with a PubMed ID number
+                            <span className="points">+ 1 points</span>
+                            </p>
+                        </label>
+
+                        <input
+                            type="radio"
+                            name="rGroup"
+                            value={2}
+                            id="r3"
+                            onChange={this.props.handleChange('publications')}
+                            defaultChecked={handleDefaultChecked(this.props.values.publications, 2)} />
+                        <label className="radio" htmlFor="r3">
+                            <p><strong>More than 1</strong> Educational research paper published in a peer-reviewed journal with a PubMed ID number
+                            <span className="points">+ 2 points</span>
+                            </p>
+                        </label>
+                    </div>
                 </div>
                 <Button onClick={this.back} className="formBtn" animated>
                     <Button.Content visible>Back</Button.Content>
